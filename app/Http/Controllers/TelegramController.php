aba924dc21c01fc7b255b2e20b2a811fc1c538fa
Bertho <bertho@pawoon.com>
2019-03-12 02:44:41 +0700

    Change using facade

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
     public function me()
     {
-        $client = new \GuzzleHttp\Client();
-        $me = "https://api.telegram.org/bot707434480:AAGsDoc4tSudDB1F4nWKGXYKDyQxHi4tL7A/getMe";
-        $response = $client->post($me);
-        return response()->json($response->getBody());
+        // $client = new \GuzzleHttp\Client();
+        // $me = "https://api.telegram.org/bot707434480:AAGsDoc4tSudDB1F4nWKGXYKDyQxHi4tL7A/getMe";
+        // $response = $client->post($me);
+        // return response()->json($response->getBody());
+
+        $response = Telegram::getMe();
+
+        $botId = $response->getId();
+        $firstName = $response->getFirstName();
+        $username = $response->getUsername();
+
+        return response()->json($botId);
     }
 }
