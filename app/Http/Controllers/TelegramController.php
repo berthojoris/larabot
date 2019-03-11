a4f7acafd9580d34ddceccadb8f319b760006b66
Bertho <bertho@pawoon.com>
2019-03-12 02:33:52 +0700

    Using real bot token

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -8,7 +8,7 @@ class TelegramController extends Controller
 {
     public function me()
     {
-        $me = "https://api.telegram.org/bot[["+env('TELEGRAM_BOT_TOKEN')+"]]/getMe";
+        $me = "https://api.telegram.org/bot[[707434480:AAGsDoc4tSudDB1F4nWKGXYKDyQxHi4tL7A]]/getMe";
         $response = $client->post($me);
         return $response;
     }
