0775d83ef2cbeb2e031c34b0a20fbfa48495eaf7
Bertho <bertho@pawoon.com>
2019-03-12 02:36:38 +0700

    Change endpoint

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,7 +9,7 @@ class TelegramController extends Controller
     public function me()
     {
         $client = new \GuzzleHttp\Client();
-        $me = "https://api.telegram.org/bot[[707434480:AAGsDoc4tSudDB1F4nWKGXYKDyQxHi4tL7A]]/getMe";
+        $me = "https://api.telegram.org/bot707434480:AAGsDoc4tSudDB1F4nWKGXYKDyQxHi4tL7A/getMe";
         $response = $client->post($me);
         return $response;
     }
