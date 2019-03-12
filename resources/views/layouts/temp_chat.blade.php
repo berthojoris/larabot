72427b371ea2bf7cf9c4d13211aa315603e2270b
Bertho <bertho@pawoon.com>
2019-03-12 13:47:13 +0700

    Add template chat

---
+++ resources/views/layouts/temp_chat.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat Template</title>
+    <link href='{{ asset('css/fontchat.css') }}' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="{{ asset('css/chat.css') }}">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
