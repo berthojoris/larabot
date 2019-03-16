8a5b8790e49bbd5998cb6f87b93ca5771e2e91e6
Bertho <bertho@pawoon.com>
2019-03-16 13:14:35 +0700

    Change faker seed to use static counter

--- database/seeds/NewsTableSeeder.php
+++ database/seeds/NewsTableSeeder.php
@@ -11,9 +11,9 @@ class NewsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = (int)$this->command->ask('How many news do you need ?', 10);
-        $this->command->info("Creating {$count} genres.");
-        $genres = factory(App\News::class, $count)->create();
-        $this->command->info('Genres Created!');
+        // $count = (int)$this->command->ask('How many news do you need ?', 10);
+        // $this->command->info("Creating {$count} genres.");
+        $genres = factory(App\News::class, 100)->create();
+        $this->command->info('News Created!');
     }
 }
