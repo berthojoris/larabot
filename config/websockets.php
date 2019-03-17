f9e61469039f5ba2801e3e5f992c2371edbfbae8
Bertho <bertho@pawoon.com>
2019-03-18 01:44:52 +0700

    Update local_cert at websockets.php when using SSL

--- config/websockets.php
+++ config/websockets.php
@@ -98,7 +98,7 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => null,
+        'local_cert' => '/etc/nginx/ssl/transvision.me/512577/server.crt',
 
         /*
          * Path to local private key file on filesystem in case of separate files for
