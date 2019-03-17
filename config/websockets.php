4a4da18551464259356c9896e4f7fd0c54fbee25
Bertho <bertho@pawoon.com>
2019-03-18 02:21:54 +0700

    SSL set null

--- config/websockets.php
+++ config/websockets.php
@@ -98,13 +98,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => '/etc/nginx/ssl/transvision.me/512577/server.crt',
+        'local_cert' => null,
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => '/etc/nginx/ssl/transvision.me/512577/server.key',
+        'local_pk' => null,
 
         /*
          * Passphrase for your local_cert file.
